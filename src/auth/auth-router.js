0e5e4ad06fe2a5fc917eed0754867b47365dbf42
“knoweski <Krystle.noweski@gmail.com ~$ git config --global user.name knoweski>
2020-01-09 15:50:53 -0500

    update

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -9,6 +9,7 @@ authRouter
   .post('/login', jsonBodyParser, (req, res, next) => {
     const { user_name, password } = req.body
     const loginUser = { user_name, password }
+    console.log(loginUser);
 
     for (const [key, value] of Object.entries(loginUser))
       if (value == null)
@@ -35,8 +36,8 @@ authRouter
 
             const sub = dbUser.user_name
             const payload = { user_id: dbUser.id }
-            res.send({
-              authToken: AuthService.createJwt(sub, payload),jk
+            res.json({
+              authToken: AuthService.createJwt(sub, payload)
             })
           })
       })
